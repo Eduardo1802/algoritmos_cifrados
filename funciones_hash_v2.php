58b8a56f51ff5c5e6fb483f6cb03644310826902
Eduardo1802 <115453929+Eduardo1802@users.noreply.github.com>
2022-10-18 00:39:34 -0500

    Add files via upload

---
+++ funciones_hash_v2.php
@@ -0,0 +1,56 @@
+<?php
+    include("conexion.php");
+
+    if(isset($_POST['registrar'])){
+        if(strlen($_POST['nombre']) >= 1 && strlen($_POST['apellido']) >= 1 && strlen($_POST['edad']) >= 1 && strlen($_POST['email']) >= 1 && strlen($_POST['clave']) >= 1 ){
+            $nombre=trim($_POST['nombre']);
+            $apellido=trim($_POST['apellido']);
+            $edad=trim($_POST['edad']);
+            $email =trim($_POST['email']);
+            $clave=trim($_POST['clave']);
+            // $hash_v2 = hash("sha256",$clave);
+            $hash_v2 = sha1($clave);
+            
+            $consulta = "INSERT INTO tbl_hash_v2(nombre,apellido,edad,correo,contra,contra2) VALUES ('$nombre','$apellido','$edad','$email','$hash_v2','$clave');";
+            $resultado = mysqli_query($conn,$consulta);
+            if($resultado){
+                $url = "http://localhost/algoritmos/cifrado_hash_v2.php";
+                $contenido = file_get_contents($url);
+                echo $contenido;
+            }else{
+                 ?><h3>Ups a ocurrido un error</h3>
+                 <?php
+            }
+        }else{
+            ?><h3>¡Por Favor complete todos los campos!</h3> 
+                <?php
+        }        
+    }
+
+    if(isset($_POST['verificar'])){
+        if(strlen($_POST['clave']) >= 1 && strlen($_POST['clave_no']) >= 1){
+            $clave=trim($_POST['clave']);
+            $clave_no=trim($_POST['clave_no']);
+            
+            // $consulta = "SELECT contra,contra2 FROM tbl_hash_v1 WHERE contra = '4b87c5d0f7f4be62c8f5381104339e01' AND contra2= 'nadhi123'";
+            $consulta = "SELECT contra,contra2 FROM tbl_hash_v2 WHERE contra = '$clave' AND contra2= '$clave_no'";
+            $resultado = mysqli_query($conn,$consulta);
+            
+            if(!$resultado){
+                echo "<center style='margin-top: 15%;'><h1 style='justif: center;color: #1a2537;font-size: 40px;'>Datos incorrectos!</h1>";
+                echo"<a href='cifrado_hash_v2.php' style='text-align: center;color: red;font-size: 40px;'> Regresar</a></center>";        
+            }
+
+            $row = mysqli_fetch_row($resultado);
+                     
+            if($row[0] == $clave && $row[1] == $clave_no){
+                echo "<center style='margin-top: 15%;'><h1 style='justif: center;color: green;font-size: 40px;'>La contraseña es correcta!</h1>";
+                echo"<a href='cifrado_hash_v2.php' style='text-align: center;color: #1a2537;font-size: 40px;'> Regresar</a></center>";       
+            }else{
+                echo "<center style='margin-top: 15%;'><h1 style='justif: center;color: red;font-size: 40px;'>Ups la contraseña es Incorrecta!</h1>";
+                echo"<a href='cifrado_hash_v2.php' style='text-align: center;color: #1a2537;font-size: 40px;'> Regresar</a></center>";        
+            }   
+        }      
+    }
+
+?>
---
+++ funciones_rsa.php
@@ -0,0 +1,56 @@
+<?php
+include("conexion.php");
+
+$configargs=array(
+    "config"=> "C:/xampp/php/extras/openssl/openssl.cnf", // Argumentos para generar las llaves
+    // "config"=> "/opt/alt/openssl11/etc/pki/tls/openssl.cnf",
+    'private_key_bits'=>2048,
+    'default_md'=>"sha256",
+);
+if(isset($_POST['registrar'])){
+    if(strlen($_POST['nombre']) >= 1 && strlen($_POST['apellido']) >= 1 && strlen($_POST['edad']) >= 1 && strlen($_POST['email']) >= 1 && strlen($_POST['clave']) >= 1 ){
+        $generar=openssl_pkey_new($configargs); // Creacion de las dos llaves 
+        openssl_pkey_export($generar,$keyprivada, NULL, $configargs); // Exporta el contenido de la llave privada a la variable $keyprivada
+        $keypub=openssl_pkey_get_details($generar);//Obtiene los detalles de la llave para generar la llave publica.
+       
+        $nombre=trim($_POST['nombre']);
+        $apellido=trim($_POST['apellido']);
+        $edad=trim($_POST['edad']);
+        $email =trim($_POST['email']);
+        $clave=trim($_POST['clave']);
+        $keypublica = $keypub['key']; 
+
+        openssl_public_encrypt($clave,$datos_cifrados,$keypublica);  // Metodo para cifrar los datos
+        
+        $dato=base64_encode($datos_cifrados);
+        $dato2=base64_decode($dato);
+
+        openssl_private_decrypt($dato2,$datos_decifrados); // Metodo para decifrar los datos
+        
+        // echo"Key privada: ".$keyprivada."<br><br>";
+        // echo"Key publica: ".$keypublica."<br><br>"; 
+        // echo"Email: ".$email."<br><br>";
+        // echo "Datos cifrados: ".$datos_cifrados."<br><br>";
+        // echo "Datos decifrados: ".$datos_decifrados."";
+        $consulta = "INSERT INTO tbl_rsa(nombre,apellido,edad,_key,_public,correo,contra,contra2) VALUES ('$nombre','$apellido','$edad','$keyprivada','$keypublica','$email','$dato','$datos_decifrados');";
+        $resultado = mysqli_query($conn,$consulta);
+        if($resultado){
+            $url = "http://localhost/algoritmos/cifrado_rsa.php";
+            $contenido = file_get_contents($url);
+            echo $contenido;
+        }else{
+             ?><h3>Ups a ocurrido un error</h3>
+             <?php
+        }   
+    }
+}
+
+if(isset($_POST['cifrar'])){
+    if(strlen($_POST['clave']) >= 1){ 
+        $datos_cifrados =trim($_POST['clave']); 
+        $dato2=base64_decode($datos_cifrados);
+        openssl_private_decrypt($dato2,$datos_decifrados); // Metodo para decifrar los datos
+        echo "<center style='margin-top: 15%;'><h1 style='justif: center;color: #1a2537;font-size: 40px;'>La contraseña es: ".$datos_decifrados."</h1>";
+        echo"<a href='cifrado_aes.php' style='text-align: center;color: #1a2537;font-size: 40px;'> Regresar</a></center>";    
+    }
+}
